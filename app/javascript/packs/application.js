c4b8b9bcd844f6778cbbca26f844c94b9ef17fbb
Aya Koffi <ayajulie@gmail.com>
2021-06-06 08:28:12 +0200

    Replaces the versions of poppers and bootsrap to fit with version 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// to import boostrap 
+import 'bootstrap'
+
+
+
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
+//what js module to use in the entire application
 const { environment } = require('@rails/webpacker')
-
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
+}))
 module.exports = environment
